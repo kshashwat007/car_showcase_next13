498df956b2e3171691dee8b5130ed5272b87b488
kshashwat007 <kshashwat007@gmail.com>
2023-06-23 20:51:12 +0530

    tailwind default style. hero section

---
+++ components/CustomButton.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import React from 'react'
+import Image from 'next/image'
+import { CustomButtonProps } from '@/types'
+
+const CustomButton = ({title,containerStyle,handleClick}:CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyle}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>
+        {title}
+      </span>
+    </button>
+  )
+}
+
+export default CustomButton
